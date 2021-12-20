224811323dfe36ebcb5491969b0ffee8e15b4d93
Sgenne <simon.genne@gmail.com>
2021-12-20 20:32:34 +0100

    blog posts are now fetched from AG-backend

--- src/components/blog/BlogPost.js
+++ src/components/blog/BlogPost.js
@@ -3,10 +3,9 @@ import parser from "html-react-parser";
 import styles from "./BlogPost.module.css";
 
 const BlogPost = ({ post }) => {
-  //const [displayPost, setDisplay] = useState(false)
 
 
-  const date = new Date(post.timestamp);
+  const date = new Date(post.createdAt);
   const [day, month, year] = [date.getDate(), date.getMonth(), date.getFullYear()];
 
 
@@ -14,7 +13,6 @@ const BlogPost = ({ post }) => {
     <div className={styles["post"]}>
       <header className={styles["post-header"]}>
         <h1 className={styles["post-header__title"]}>{post.title}</h1>
-        <p className={styles["post-header__category"]}>Macro</p>
         <p className={styles["post-header__timestamp"]}>{`${day}/${month}/${year}`}</p>
       </header>
       <main className={styles["post-content"]}>
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -3,21 +3,26 @@ import { useState, useEffect, useContext } from "react";
 import Blog from "../components/blog/Blog";
 
 import FirebaseContext from "../store/firebase-context";
+import useBackend from "../hooks/use-backend";
 
 const NUMBER_OF_POSTS_TO_LOAD = 4;
 
 const BlogPage = () => {
   const [posts, setPosts] = useState([]);
 
-  const firebaseContext = useContext(FirebaseContext);
+  const { getAllBlogPosts } = useBackend();
 
   useEffect(() => {
-    const fetchPosts = async () => {
-      const fetchedPosts = await firebaseContext.getAllBlogPosts();
-      setPosts(fetchedPosts.posts.map((post) => ({ ...post, display: false })));
-    };
-    fetchPosts();
-  }, [firebaseContext]);
+    getAllBlogPosts((result) => {
+      setPosts(result.blogPosts)
+    })
+    
+    // const fetchPosts = async () => {
+    //   const fetchedPosts = await getAllBlogPosts();
+    //   setPosts(fetchedPosts.posts.map((post) => ({ ...post, display: false })));
+    // };
+    // fetchPosts();
+  }, [getAllBlogPosts]);
 
   return <Blog numberOfPostsToLoad={NUMBER_OF_POSTS_TO_LOAD} posts={posts} />;
 };
